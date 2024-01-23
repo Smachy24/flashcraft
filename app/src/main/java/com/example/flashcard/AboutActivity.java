3415a58c4e55066ab4db9d9b744c6731cd84694e
Daadaa Nabil <103863031+RyzKum@users.noreply.github.com>
2024-01-23 23:25:29 +0100

    Ajout de l'affichage de la version de l'application sur la page ABOUT
    Ajout de l'affichage de la version de l'application sur la page ABOUT de façon dynamique

--- app/src/main/java/com/example/flashcard/AboutActivity.java
+++ app/src/main/java/com/example/flashcard/AboutActivity.java
@@ -2,7 +2,10 @@ package com.example.flashcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView aboutVersion  = findViewById(R.id.aboutVersion);
+
+        Context context = getApplicationContext();
+        PackageManager packageManager = context.getPackageManager();
+        String packageName = context.getPackageName();
+
+        String myVersionName = "not available";
+
+        try {
+            myVersionName = packageManager.getPackageInfo(packageName, 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        aboutVersion.setText(myVersionName);
+
+
     }
 }
