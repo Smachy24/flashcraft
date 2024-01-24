9601e505501f72465d001a7435c708e8b28c7e4a
Smachy <pro.diegocdossantos@gmail.com>
2024-01-24 11:28:44 +0100

    docs: remove call utils on main

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Utils.Drawables.listDrawables();
-
         videoView = findViewById(R.id.videoView);
 
         MediaController mediaController = new MediaController(this);
