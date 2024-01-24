7e8245afb42292095c4a0c7631e18f1428c5f0e2
DiegoUrbyn <diego.dossantos@urbyn.co>
2024-01-24 23:25:56 +0100

    drag and drop

--- app/src/main/java/com/example/flashcard/TimeAttackActivity.java
+++ app/src/main/java/com/example/flashcard/TimeAttackActivity.java
@@ -1,8 +1,8 @@
 package com.example.flashcard;
 
-import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.Gravity;
@@ -11,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,25 +23,29 @@ public class TimeAttackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_attack);
 
-        View exempleImage = findViewById(R.id.exempleImage);
+        ImageView exempleImage = findViewById(R.id.exempleImage);
         exempleImage.setOnTouchListener(new DragItemTouchListener());
         bind();
     }
 
     private void bind() {
         FrameLayout craftingTableFrameLayout = findViewById(R.id.craftingTableFrameLayout);
-        for (int i = 0; i < 9; i++) {
+
+        for (int i = 0; i < craftingTableFrameLayout.getChildCount(); i++) {
             View child = craftingTableFrameLayout.getChildAt(i);
-            if (child instanceof FrameLayout) {
-                System.out.println(i);
-                FrameLayout craftingFrame = (FrameLayout) child;
-                craftingFrame.setOnDragListener(new DragListener());
+
+            // Vérifie si l'enfant est une instance de ViewGroup
+            if (child instanceof ImageView) {
+                ImageView craftingImageView = (ImageView) child;
+                craftingImageView.setOnDragListener(new DragListener());
             }
         }
     }
 
 
-    private class DragItemTouchListener implements View.OnTouchListener {
+
+
+    private static class DragItemTouchListener implements View.OnTouchListener {
 
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -58,7 +63,7 @@ public class TimeAttackActivity extends AppCompatActivity {
             ViewGroup parent = (ViewGroup) view.getParent();
 
             view.startDragAndDrop(data, shadowBuilder, view, 0);
-            parent.removeView(view);
+//            parent.removeView(view);
         }
     }
 
@@ -66,34 +71,23 @@ public class TimeAttackActivity extends AppCompatActivity {
 
         @Override
         public boolean onDrag(View v, DragEvent event) {
-            switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_STARTED:
-                    // Actions for drag started
-                    break;
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    // Actions for drag entered
-                    break;
-                case DragEvent.ACTION_DRAG_EXITED:
-                    // Actions for drag exited
-                    break;
-                case DragEvent.ACTION_DROP:
-                    animateDropEffect((ViewGroup) v, (View) event.getLocalState());
-                    break;
-                case DragEvent.ACTION_DRAG_ENDED:
-                    // Actions for drag ended
-                    break;
-                default:
-                    break;
+            if (event.getAction() == DragEvent.ACTION_DROP) {
+                ImageView draggedView = (ImageView) event.getLocalState();
+
+                // Vérifier si la vue cible est une instance de ImageView et si la vue draggée est l'image d'exemple
+                if (v instanceof ImageView && draggedView.getId() == R.id.exempleImage) {
+
+                    // Obtenez l'ID de la vue cible
+                     int targetImageId = v.getId();
+
+                    // Changez l'image de la vue cible pour qu'elle soit la même que celle de la vue draggée
+                    ((ImageView) v).setImageDrawable(draggedView.getDrawable());
+
+                    v.invalidate();
+                }
             }
             return true;
         }
 
-        private void animateDropEffect(ViewGroup into, View view) {
-            into.addView(view);
-            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view.getLayoutParams();
-            params.gravity = Gravity.END;
-            view.setLayoutParams(params);
-            view.setVisibility(View.VISIBLE);
-        }
     }
 }
