05d263e87c5c6df6b7c7e6f7c89badbdc66d3cd5
Noé HLM <noeheylemadec@gmail.com>
2024-01-25 10:52:24 +0100

    summary page - all data collected and show and named

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -56,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements Utils.OnQuestions
         videoView.setOnCompletionListener(mp -> videoView.start());
 
         // Launching a normal mode game.
-        Button questionButton = findViewById(R.id.questionButton);
+        Button questionButton = findViewById(R.id.homeSummaryButton);
         questionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
