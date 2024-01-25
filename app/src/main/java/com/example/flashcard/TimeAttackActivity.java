848bd4d950e1ec7435f14bc3182508f6797bb20c
Smachy24 <pro.diegocdossantos@gmail.com>
2024-01-25 19:39:36 +0100

    next craft is working

--- app/src/main/java/com/example/flashcard/TimeAttackActivity.java
+++ app/src/main/java/com/example/flashcard/TimeAttackActivity.java
@@ -1,11 +1,9 @@
 package com.example.flashcard;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.DragEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -17,7 +15,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.flashcard.models.Craft;
-import com.example.flashcard.models.Game;
 import com.example.flashcard.models.Item;
 import com.example.flashcard.models.TimeAttackGame;
 
@@ -51,7 +48,6 @@ public class TimeAttackActivity extends AppCompatActivity {
             }
         }
 
-
         RecyclerView recyclerView = findViewById(R.id.itemListRecyclerView);
         ItemAdapter adapter = new ItemAdapter(items);
         recyclerView.setAdapter(adapter);
@@ -59,14 +55,79 @@ public class TimeAttackActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         bind();
-
         initGame();
 
     }
 
-    private void playRound(Craft craft, int round){
+    private void playRound(TimeAttackGame game ){
+        ArrayList<Craft>craftList = game.getCrafts();
+        int totalCrafts = craftList.size();
+        int round = game.getCurrentCraftIndex();
+
+        if(round < totalCrafts){
+            Craft craft = craftList.get(round);
+            TextView itemCraftedTextView = findViewById(R.id.itemCraftedTextView);
+            itemCraftedTextView.setText(craft.getCraftedItemName());
+
+            System.out.println("----------------------");
+            System.out.println(craft.getCraftedItemName());
+            System.out.println("----------------------");
+
+            ImageView itemCraftedImageView = findViewById(R.id.itemCraftedImageView);
+            itemCraftedImageView.setImageResource(craft.getCraftedItemImage());
+
+            TextView currentCraftTextView = findViewById(R.id.currentCraftTextView);
+            currentCraftTextView.setText("Craft : " + (round+1));
+
+            TextView totalCraftTextView = findViewById(R.id.totalCraftTextView);
+            totalCraftTextView.setText(" / " + totalCrafts);
+
+            Button validateCraftButton = findViewById(R.id.validateCraftButton);
+            TextView timerTextView = findViewById(R.id.timerTextView);
+
+            CountDownTimer countDownTimer = new CountDownTimer(30000, 1000) { // 30 secondes avec des mises à jour toutes les 1 seconde
+                public void onTick(long millisUntilFinished) {
+                    timerTextView.setText(millisUntilFinished / 1000 + "s");
+                }
+
+                public void onFinish() {
+                    timerTextView.setText("Temps écoulé !");
+
+                }
+            };
+
+            // Démarrez le compte à rebours
+            countDownTimer.start();
+
+            validateCraftButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean craftMatch = checkCraft(craft, countDownTimer);
+                    if(craftMatch){
+                        game.setCurrentCraftIndex(round+1);
+                        resetGrid();
+                        playRound(game);
+                    }
+                }
+            });
+        }
+
+        else {
+            startActivity(new Intent(TimeAttackActivity.this, MainActivity.class));
+        }
 
     }
+
+    private void resetGrid() {
+        for (int i = 0; i < 9; i++) {
+            int resourceId = getResources().getIdentifier("craftingSlotImage" + i, "id", getPackageName());
+            ImageView slot = findViewById(resourceId);
+            slot.setImageResource(R.drawable.crafting_table_slot);
+            slot.setTag("empty");
+        }
+    }
+
+
 
     private void initGame(){
         Intent srcIntent = getIntent();
@@ -74,48 +135,16 @@ public class TimeAttackActivity extends AppCompatActivity {
 
         ArrayList<Craft> craftsList = game.getCrafts();
 
+
         int currentCraftIndex = game.getCurrentCraftIndex();
         Craft currentCraft = craftsList.get(currentCraftIndex);
+        playRound(game);
 
-        TextView itemCraftedTextView = findViewById(R.id.itemCraftedTextView);
-        itemCraftedTextView.setText(currentCraft.getCraftedItemName());
 
-        ImageView itemCraftedImageView = findViewById(R.id.itemCraftedImageView);
-        itemCraftedImageView.setImageResource(currentCraft.getCraftedItemImage());
-
-        TextView currentCraftTextView = findViewById(R.id.currentCraftTextView);
-        currentCraftTextView.setText("Craft : " + (currentCraftIndex+1));
-
-        TextView totalCraftTextView = findViewById(R.id.totalCraftTextView);
-        totalCraftTextView.setText(" / " + craftsList.size());
-
-        Button validateCraftButton = findViewById(R.id.validateCraftButton);
-
-        TextView timerTextView = findViewById(R.id.timerTextView);
-        CountDownTimer countDownTimer = new CountDownTimer(30000, 1000) { // 30 secondes avec des mises à jour toutes les 1 seconde
-            public void onTick(long millisUntilFinished) {
-                timerTextView.setText(millisUntilFinished / 1000 + "s");
-            }
-
-            public void onFinish() {
-                timerTextView.setText("Temps écoulé !");
-
-            }
-        };
-
-        // Démarrez le compte à rebours
-        countDownTimer.start();
-
-        validateCraftButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkCraft(currentCraft, countDownTimer);
-            }
-        });
 
     }
 
-    private void checkCraft(Craft goodCraft, CountDownTimer timer) {
+    private boolean checkCraft(Craft goodCraft, CountDownTimer timer) {
         boolean craftMatch = true;
 
         for (int i = 0; i <= 8; i++) {
@@ -132,10 +161,10 @@ public class TimeAttackActivity extends AppCompatActivity {
         if (craftMatch) {
             System.out.println("Le craft est bon");
             timer.cancel();
-        } else {
-            System.out.println("Le craft est erroné");
+            return true;
         }
-
+        System.out.println("Le craft est erroné");
+        return false;
 
     }
 
