11cb7f6bb68aa3407be8be24c156bfdcbefe48cb
Noé HLM <noeheylemadec@gmail.com>
2024-01-25 20:24:33 +0100

    green mark success page

--- app/src/main/java/com/example/flashcard/SuccessActivity.java
+++ app/src/main/java/com/example/flashcard/SuccessActivity.java
@@ -1,6 +1,7 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,16 @@ public class SuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        ImageView greenMarkNetherImageView = findViewById(R.id.greenMarkNetherImageView);
+        ImageView greenMarkEndImageView = findViewById(R.id.greenMarkEndImageView);
+        ImageView greenMarkEnderDragonImageView = findViewById(R.id.greenMarkEnderDragonImageView);
+        ImageView greenMarkWitherImageView = findViewById(R.id.greenMarkWitherImageView);
+        ImageView greenMarkWardenImageView = findViewById(R.id.greenMarkWardenImageView);
+        ImageView greenMarkWaterTempleImageView = findViewById(R.id.greenMarkWaterTempleImageView);
+        ImageView greenMarkGuardianImageView = findViewById(R.id.greenMarkGuardianImageView);
+        ImageView greenMarkAetherImageView = findViewById(R.id.greenMarkEatherImageView);
+        ImageView greenMarkGodImageView = findViewById(R.id.greenMarkGodImageView);
     }
+
 }
