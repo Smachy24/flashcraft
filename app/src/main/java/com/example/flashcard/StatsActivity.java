2afbf28f9e037965fe7c2f8de237250adbc47907
Noé HLM <noeheylemadec@gmail.com>
2024-01-25 15:09:28 +0100

    stats setup

--- app/src/main/java/com/example/flashcard/StatsActivity.java
+++ app/src/main/java/com/example/flashcard/StatsActivity.java
@@ -1,8 +1,11 @@
 package com.example.flashcard;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        //Récupération des TextView
+        TextView totalQuestionsTextView = findViewById(R.id.totalQuestionsTextView);
+        TextView totalGoodAnswersTextView = findViewById(R.id.totalGoodAnswersTextView);
+        TextView globalPercentage = findViewById(R.id.globalPercentageTextView);
+
+        //Récupération des SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("GlobalStatistics", MODE_PRIVATE);
+
+        int totalQuestions = sharedPreferences.getInt("TotalQuestions", 0);
+        int totalGoodAnswers = sharedPreferences.getInt("TotalGoodAnswers", 0);
+
+        Log.i("testtotal", String.valueOf(totalQuestions));
+        Log.i("testgood", String.valueOf(totalGoodAnswers));
+
+        //Mise en place des scores globaux
+        totalQuestionsTextView.setText(String.valueOf(totalQuestions));
+        totalGoodAnswersTextView.setText(String.valueOf(totalGoodAnswers));
+        globalPercentage.setText(totalGoodAnswers*100/totalQuestions+" %");
     }
 }
