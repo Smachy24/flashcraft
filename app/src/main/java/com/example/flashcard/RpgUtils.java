70becb3f740155712f70dff0bab182168c42072d
jooob56 <testdujourproton@protonmail.com>
2024-01-25 19:24:46 +0100

    Add basic gameplay loop with question chaining and stats update

--- app/src/main/java/com/example/flashcard/RpgUtils.java
+++ app/src/main/java/com/example/flashcard/RpgUtils.java
@@ -62,8 +62,9 @@ public class RpgUtils {
 
     public static ArrayList<RpgQuestion> getBaseQuestions()
     {
+        // new base question
         ArrayList<RpgQuestion> baseQuestions = new ArrayList<>();
-        ArrayList<RpgAnswer> a1 = new ArrayList<RpgAnswer>() {{
+        ArrayList<RpgAnswer> answersBaseQuestion1 = new ArrayList<RpgAnswer>() {{
             add(new RpgAnswer(
                     "Tu prends tout pour toi, après tout, c'est la survie",
                     -5, 0, 1, 0,
@@ -86,22 +87,42 @@ public class RpgUtils {
                     "Le village",
                     "Tu trouves un village abandonné avec des coffres remplis de provisions. Que fais-tu ?",
                     "La survie peut être difficile, mais l'empathie envers les autres est essentielle pour préserver notre humanité.",
-                    a1));
+                    answersBaseQuestion1));
 
+
+        // new base question
+        ArrayList<RpgAnswer> answersBaseQuestion2 = new ArrayList<RpgAnswer>() {{
+            add(new RpgAnswer(
+                    "Tu respectes les villageois et cherches des diamants ailleurs",
+                    +5, 0, -5, 0,
+                    0, 0, 0, 2
+            ));
+            add(new RpgAnswer(
+                    "Tu les attaques pour obtenir les diamants, justifiant cela par la nécessité",
+                    -5, 0, +5, +10,
+                    5, 0, 0, 0
+            ));
+            add(new RpgAnswer(
+                    "Tu essaies de négocier avec eux pour partager les ressources",
+                    +1, 0, 0, -1,
+                    2, 0, 0, 0
+            ));
+        }};
 
         baseQuestions.add(
                 new RpgQuestion(
                         "Le pouvoir de la camaraderie",
                         "Un compagnon joueur est en difficulté et demande ton aide. Comment réagis-tu ?",
                         "L'amitié et l'altruisme sont des éléments clés dans la construction d'une communauté forte.",
-                        a1));
+                        answersBaseQuestion2));
 
+        // new base question
         baseQuestions.add(
                 new RpgQuestion(
                         "C'est une mine !",
                         "Tu découvres une mine de diamants, mais elle est protégée par des villageois pacifiques. Que fais-tu ?",
                         "Les richesses obtenues par l'injustice ne mènent qu'à une fausse prospérité",
-                        a1));
+                        answersBaseQuestion1));
 
         return baseQuestions;
     }
