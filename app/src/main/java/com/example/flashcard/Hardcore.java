8fab4b3d6a26aaec9670b2e5235068971f2fcad0
jooob56 <testdujourproton@protonmail.com>
2024-01-25 10:27:44 +0100

    Add base layout structure for hardcore mode

--- app/src/main/java/com/example/flashcard/Hardcore.java
+++ app/src/main/java/com/example/flashcard/Hardcore.java
@@ -3,12 +3,30 @@ package com.example.flashcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class Hardcore extends AppCompatActivity {
 
+    ProgressBar lifeBar;
+    ProgressBar hungerBar;
+    ProgressBar powerBar;
+    ProgressBar stressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hardcore);
+
+        lifeBar = findViewById(R.id.lifeProgressBar);
+        hungerBar = findViewById(R.id.hungerProgressBar);
+        powerBar = findViewById(R.id.powerProgressBar);
+        stressBar = findViewById(R.id.stressProgressBar);
+
+        // default value assignation
+        lifeBar.setProgress(25);
+        hungerBar.setProgress(25);
+        powerBar.setProgress(25);
+        stressBar.setProgress(25);
+
+
     }
 }
