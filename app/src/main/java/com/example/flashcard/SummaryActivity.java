9e765d7624f2ab1ece36b35bb80f35613bf81472
Noé HLM <noeheylemadec@gmail.com>
2024-01-25 10:46:23 +0100

    summary page - all data collected and show

--- app/src/main/java/com/example/flashcard/SummaryActivity.java
+++ app/src/main/java/com/example/flashcard/SummaryActivity.java
@@ -1,8 +1,13 @@
 package com.example.flashcard;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.flashcard.models.Game;
 
 public class SummaryActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class SummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+
+        //Récupération de l'Intent
+        Intent srcIntent = getIntent();
+        String difficulty = srcIntent.getStringExtra("Difficulty");
+        Log.i("difficulty", difficulty);
+        Game game = srcIntent.getParcelableExtra("game");
+
+        //Récupération des TextView
+        TextView difficultyLevel = findViewById(R.id.difficultyLevelTextView);
+        TextView numberGoodAnswers = findViewById(R.id.good_answersTextView);
+        TextView percentageGoodAnswers = findViewById(R.id.percentageTextView);
+
+        //Mise en place des scores dans les TextView
+        difficultyLevel.setText("Niveau de difficulté : " + difficulty);
+        numberGoodAnswers.setText(game.getScore()+" / "+game.getQuestions().size()+" bonne(s) réponse(s)");
+        int percentage = game.getScore() * 100 / game.getQuestions().size();
+        percentageGoodAnswers.setText(percentage+" %");
+
+
     }
 }
