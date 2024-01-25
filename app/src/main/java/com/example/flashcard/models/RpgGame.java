a2c5923aa33042b672e77eff2c338cdd9e0d7d64
jooob56 <testdujourproton@protonmail.com>
2024-01-25 13:01:09 +0100

    update rpgGame model

--- app/src/main/java/com/example/flashcard/models/RpgGame.java
+++ app/src/main/java/com/example/flashcard/models/RpgGame.java
@@ -1,5 +1,7 @@
 package com.example.flashcard.models;
 
+import com.example.flashcard.RpgUtils;
+
 import java.util.ArrayList;
 
 public class RpgGame {
@@ -8,8 +10,41 @@ public class RpgGame {
     private int score;
     private RpgPlayer player;
 
+    public RpgGame(String difficulty)
+    {
+        this.player = new RpgPlayer();
+        this.questions = RpgUtils.getBaseQuestions();
+        this.score = 0;
+
+        this.difficulty = difficulty;
+    }
 
 
+    public String getDifficulty() {
+        return difficulty;
+    }
 
+    public ArrayList<RpgQuestion> getQuestions() {
+        return questions;
+    }
 
+    public void setQuestions(ArrayList<RpgQuestion> questions) {
+        this.questions = questions;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public RpgPlayer getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(RpgPlayer player) {
+        this.player = player;
+    }
 }
