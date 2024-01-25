e066d106a5ab630103ffc74b5bd5397275a95504
Noé HLM <noeheylemadec@gmail.com>
2024-01-26 00:47:49 +0100

    losing and winning time attack UI

--- app/src/main/java/com/example/flashcard/TimeAttackScores.java
+++ app/src/main/java/com/example/flashcard/TimeAttackScores.java
@@ -1,6 +1,10 @@
 package com.example.flashcard;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,15 @@ public class TimeAttackScores extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_attack_scores);
+
+        Button home = findViewById(R.id.MainMenuLosingButton);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final MediaPlayer mediaPlayer = MediaPlayer.create(SummaryActivity.this,R.raw.click_sound);
+                mediaPlayer.start();
+                startActivity(new Intent(TimeAttackScores.this, MainActivity .class));
+            }
+        });
     }
 }
