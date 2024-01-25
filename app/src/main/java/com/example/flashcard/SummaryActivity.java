623280505577117c396b39d771477928ef40799d
Noé HLM <noeheylemadec@gmail.com>
2024-01-25 11:05:38 +0100

    button home

--- app/src/main/java/com/example/flashcard/SummaryActivity.java
+++ app/src/main/java/com/example/flashcard/SummaryActivity.java
@@ -1,8 +1,11 @@
 package com.example.flashcard;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,12 +30,25 @@ public class SummaryActivity extends AppCompatActivity {
         TextView difficultyLevel = findViewById(R.id.difficultyLevelTextView);
         TextView numberGoodAnswers = findViewById(R.id.good_answersTextView);
         TextView percentageGoodAnswers = findViewById(R.id.percentageTextView);
+        Button homeButton = findViewById(R.id.homeSummaryButton);
 
         //Mise en place des scores dans les TextView
         difficultyLevel.setText("Niveau de difficulté : " + difficulty);
         numberGoodAnswers.setText(game.getScore()+" / "+game.getQuestions().size()+" bonne(s) réponse(s)");
         int percentage = game.getScore() * 100 / game.getQuestions().size();
         percentageGoodAnswers.setText(percentage+" %");
+
+        //Retour au menu principal
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final MediaPlayer mediaPlayer = MediaPlayer.create(SummaryActivity.this,R.raw.click_sound);
+                mediaPlayer.start();
+                startActivity(new Intent(SummaryActivity.this, MainActivity .class));
+            }
+        });
+
+
 
 
     }
